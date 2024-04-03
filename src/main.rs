cd985cb11110098ba8c9cd2ca14cfc06f730c5c3
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-04-03 23:01:33 +0800

    wawa

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Position {x: f32, y: f32}
+struct Entity(u64);
+
+fn hello_world() {
+    println!("hello world");
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_systems(Update, hello_world)
+    .run();
 }
